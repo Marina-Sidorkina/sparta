22bc5391e7f363f8ed141647865d4d87de1fff67
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-03-19 23:36:08 +0300

    Gulp: 1) Add gulp-concat/ gulp-uglify-es/ gulp-sourcemaps to devDependencies 2) Add task for scripts in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sourcemaps = require("gulp-sourcemaps");
 
 // Server
 
@@ -11,4 +14,19 @@ function browsersync() {
 	})
 }
 
+// Scripts
+
+function scripts() {
+	return src([
+		'source/js/index.js'
+		])
+  .pipe(sourcemaps.init())
+	.pipe(concat('index.min.js'))
+	.pipe(uglify())
+  .pipe(sourcemaps.write('.'))
+	.pipe(dest('source/js/'))
+	.pipe(browserSync.stream())
+}
+
 exports.browsersync = browsersync;
+exports.scripts = scripts;
