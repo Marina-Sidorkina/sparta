92edef09a88e62d88c522525f582217e2c310756
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-03-20 00:51:13 +0300

    Change quotes in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-const { src, dest, parallel, series, watch } = require('gulp');
-const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
+const { src, dest, parallel, series, watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
 const sourcemaps = require("gulp-sourcemaps");
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cleancss = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
-const newer = require('gulp-newer');
-const del = require('del');
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cleancss = require("gulp-clean-css");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const del = require("del");
 
 // Server
 
 function browsersync() {
 	browserSync.init({
-		server: { baseDir: 'source/' },
+		server: { baseDir: "source/" },
 		notify: false,
 		online: true
 	})
@@ -24,25 +24,25 @@ function browsersync() {
 
 function scripts() {
 	return src([
-		'source/js/index.js'
+		"source/js/index.js"
 		])
   .pipe(sourcemaps.init())
-	.pipe(concat('index.min.js'))
+	.pipe(concat("index.min.js"))
 	.pipe(uglify())
-  .pipe(sourcemaps.write('.'))
-	.pipe(dest('source/js/'))
+  .pipe(sourcemaps.write("."))
+	.pipe(dest("source/js/"))
 	.pipe(browserSync.stream())
 }
 
 // Styles
 
 function styles() {
-	return src('source/sass/index.scss')
+	return src("source/sass/index.scss")
   .pipe(sourcemaps.init())
 	.pipe(sass())
-	.pipe(concat('index.min.css'))
+	.pipe(concat("index.min.css"))
 	.pipe(autoprefixer({
-    overrideBrowserslist: ['last 10 versions'],
+    overrideBrowserslist: ["last 10 versions"],
     grid: true
   }))
 	.pipe(cleancss({
@@ -52,47 +52,47 @@ function styles() {
       }
     }
   }))
-  .pipe(sourcemaps.write('.'))
-	.pipe(dest('source/css/'))
+  .pipe(sourcemaps.write("."))
+	.pipe(dest("source/css/"))
 	.pipe(browserSync.stream())
 }
 
 // Images
 
 function images() {
-	return src('source/images/src/**/*')
-	.pipe(newer('source/images/dest/'))
+	return src("source/images/src/**/*")
+	.pipe(newer("source/images/dest/"))
 	.pipe(imagemin())
-	.pipe(dest('source/images/dest/'))
+	.pipe(dest("source/images/dest/"))
 }
 
 function cleanimg() {
-	return del('source/images/dest/**/*', { force: true })
+	return del("source/images/dest/**/*", { force: true })
 }
 
 // Build
 
 function buildcopy() {
 	return src([
-		'source/css/**/*.min.css',
-		'source/js/**/*.min.js',
-		'source/images/dest/**/*',
-		'source/**/*.html',
-		], { base: 'source' })
-	.pipe(dest('build'))
+		"source/css/**/*.min.css",
+		"source/js/**/*.min.js",
+		"source/images/dest/**/*",
+		"source/**/*.html",
+		], { base: "source" })
+	.pipe(dest("build"))
 }
 
 function cleanbuild() {
-	return del('build/**/*', { force: true })
+	return del("build/**/*", { force: true })
 }
 
 // Watcher
 
 function startwatch() {
- 	watch(['source/**/*.js', '!source/**/*.min.js'], scripts);
-  watch('source/**/sass/**/*', styles);
-  watch('source/**/*.html').on('change', browserSync.reload);
-  watch('source/images/src/**/*', images);
+ 	watch(["source/**/*.js", "!source/**/*.min.js"], scripts);
+  watch("source/**/sass/**/*", styles);
+  watch("source/**/*.html").on("change", browserSync.reload);
+  watch("source/images/src/**/*", images);
 }
 
 // Tasks
