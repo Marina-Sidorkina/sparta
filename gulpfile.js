dc90b968556e1d40bf3a7dbb6b02208b105184ec
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-03-20 00:12:40 +0300

    Gulp: 1) Add gulp-sass/ gulp-autoprefixer/ gulp-clean-css to devDependencies 2) Add task for styles in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const sourcemaps = require("gulp-sourcemaps");
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
 
 // Server
 
@@ -28,11 +31,38 @@ function scripts() {
 	.pipe(browserSync.stream())
 }
 
+// Styles
+
+function styles() {
+	return src('source/sass/index.scss')
+  .pipe(sourcemaps.init())
+	.pipe(sass())
+	.pipe(concat('index.min.css'))
+	.pipe(autoprefixer({
+    overrideBrowserslist: ['last 10 versions'],
+    grid: true
+  }))
+	.pipe(cleancss({
+    level: {
+      1: {
+        specialComments: 0
+      }
+    }
+  }))
+  .pipe(sourcemaps.write('.'))
+	.pipe(dest('source/css/'))
+	.pipe(browserSync.stream())
+}
+
+// Watcher
+
 function startwatch() {
- 	watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+ 	watch(['source/**/*.js', '!source/**/*.min.js'], scripts);
+  watch('source/**/sass/**/*', styles);
 }
 
 exports.browsersync = browsersync;
 exports.scripts = scripts;
+exports.styles = styles;
 
-exports.default = parallel(scripts, browsersync, startwatch);
+exports.default = parallel(styles, scripts, browsersync, startwatch);
