c771a8743c67141df3d4875290028339f49a1357
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-03-20 00:41:00 +0300

    Gulp: 1) Add gulp-imagemin/ gulp-newer/ del to devDependencies 2) Add task for images in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const sourcemaps = require("gulp-sourcemaps");
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleancss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const del = require('del');
 
 // Server
 
@@ -54,12 +57,26 @@ function styles() {
 	.pipe(browserSync.stream())
 }
 
+// Images
+
+function images() {
+	return src('source/images/src/**/*')
+	.pipe(newer('source/images/dest/'))
+	.pipe(imagemin())
+	.pipe(dest('source/images/dest/'))
+}
+
+function cleanimg() {
+	return del('source/images/dest/**/*', { force: true })
+}
+
 // Watcher
 
 function startwatch() {
  	watch(['source/**/*.js', '!source/**/*.min.js'], scripts);
   watch('source/**/sass/**/*', styles);
   watch('source/**/*.html').on('change', browserSync.reload);
+  watch('source/images/src/**/*', images);
 }
 
 // Tasks
@@ -67,5 +84,7 @@ function startwatch() {
 exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.styles = styles;
+exports.images = images;
+exports.cleanimg = cleanimg;
 
-exports.default = parallel(styles, scripts, browsersync, startwatch);
+exports.default = parallel(images, styles, scripts, browsersync, startwatch);
